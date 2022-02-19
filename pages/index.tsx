97f73219fb08112d8eea4d564c4a72a9a55958f3
jio-kim <now20412041@gmail.com>
2022-02-19 14:47:21 +0900

    second

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ const Home: NextPage = () => {
       <Link href="/about">
         <a>About</a>
       </Link>
+      <Link href="/Counter">
+        <a>Counter</a>
+      </Link>
     </nav>
   );
 };
